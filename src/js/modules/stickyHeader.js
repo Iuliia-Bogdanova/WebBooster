8316844fc067a01522572e6bef24db99bbaca355
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-12-08 20:08:23 +0400

    feat: add benefit section

--- src/js/modules/stickyHeader.js
+++ src/js/modules/stickyHeader.js
@@ -1,11 +1,14 @@
 export function stickyHeader() {
   window.addEventListener("scroll", () => {
     const headerTop = document.querySelector(".header-top");
+    const headerAbout = document.querySelector(".header-about");
 
     if (window.scrollY > 0) {
       headerTop.classList.add("scrolled");
+      headerAbout.classList.add("scrolled");
     } else {
       headerTop.classList.remove("scrolled");
+      headerAbout.classList.remove("scrolled");
     }
   });
 }
