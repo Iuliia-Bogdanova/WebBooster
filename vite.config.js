05a6f6890a67f09d89cca1204d78e25a8b1824fa
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-12-14 20:57:18 +0400

    refactor: returned dist folder & deleted vite.config.js & docs

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  build: {
-    base: "/iuliia-bogdanova/", // имя репозитория
-    outDir: "docs", // папка для сборки и последующего деплоя на github
-    emptyOutDir: true, // очистка папки перед сборкой
-  },
-});
