260c6e4aa601e392756631e5469b193014fc9632
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-12-14 19:54:50 +0400

    refactor: deleted dist & add emptyOutDir: true in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     outDir: "docs", // папка для сборки и последующего деплоя на github
+    emptyOutDir: true, // очистка папки перед сборкой
   },
 });
