ed40b67cd3a25270fc4175f030f1cd2b51d157f9
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2024-12-14 20:13:59 +0400

    refactor: add base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    base: "/iuliia-bogdanova/", // имя репозитория
     outDir: "docs", // папка для сборки и последующего деплоя на github
     emptyOutDir: true, // очистка папки перед сборкой
   },
